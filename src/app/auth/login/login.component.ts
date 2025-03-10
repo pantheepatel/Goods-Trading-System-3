2c2df00f809c739e07f5da7207758caafe19e911
pantheepatel <patel.panthee.18@gmail.com>
2025-03-10 17:46:10 +0530

    added routing link of register in login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common'; // Required for *ngFor
 import { FormsModule } from '@angular/forms'; // Required for [(ngModel)]
 
 @Component({
   selector: 'app-login',
   standalone: true, // Needed if using Angular Standalone Components
-  imports: [CommonModule, FormsModule], // Import necessary modules
+  imports: [CommonModule, FormsModule, RouterLink], // Import necessary modules
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -15,7 +15,7 @@ export class LoginComponent {
   password: string = '';
   showPassword: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   togglePassword() {
     this.showPassword = !this.showPassword;

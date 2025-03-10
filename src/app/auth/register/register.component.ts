9e0fd0c3e1289ff4a7c542aefd51770af8de9017
himanshi1810 <hgandhi1810@gmail.com>
2025-03-10 18:16:02 +0530

    Have to complete service with backend connection

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import { CommonModule } from '@angular/common'; // Required for *ngFor
 import { FormsModule } from '@angular/forms'; // Required for [(ngModel)]
 import { GujaratCity } from '../../core/enums/cities.enum';
 import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true, // Needed if using Angular Standalone Components
   imports: [CommonModule, FormsModule, RouterLink], // Import necessary modules
+  providers: [AuthService],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
   fullName: string = '';
   city: string = '';
   email: string = '';
@@ -27,6 +31,17 @@ export class RegisterComponent {
       city: this.city,
       email: this.email,
       password: this.password
+    });
+    const userData = { fullName: this.fullName, city: this.city, email: this.email, password: this.password };
+
+    this.authService.register(userData).subscribe({
+      next: (response) => {
+        console.log('Registration successful:', response);
+        this.router.navigate(['/auth/login']);
+      },
+      error: (err) => {
+        console.error('Registration failed:', err);
+      }
     });
   }
 }
